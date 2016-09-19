02d41db2c1dd058b1076ad39dd319df03c54b75c
Eric Momper <emomper23@knights.ucf.edu>
2016-09-18 20:26:15 -0400

    Dancing

--- unity/TestEnv/PsychVR/Assets/LeapMotionModules/UIInput/Scripts/Utilities/ToggleToggler.cs
+++ unity/TestEnv/PsychVR/Assets/LeapMotionModules/UIInput/Scripts/Utilities/ToggleToggler.cs
@@ -11,11 +11,9 @@ namespace Leap.Unity.InputModule {
 
     public void SetToggle(Toggle toggle) {
       if (toggle.isOn) {
-        text.text = "On";
         text.color = Color.white;
         image.color = OnColor;
       } else {
-        text.text = "Off";
         text.color = new Color(0.3f, 0.3f, 0.3f);
         image.color = OffColor;
       }

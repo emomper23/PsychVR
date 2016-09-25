36f7e2f95c6ea21d836fd958e1c990cb85cf6050
Eric Momper <emomper23@knights.ucf.edu>
2016-09-25 15:42:20 -0400

    fixed controller !!

--- unity/TestEnv/PsychVR/Assets/universalPrefabs/recenter.cs
+++ unity/TestEnv/PsychVR/Assets/universalPrefabs/recenter.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class recenter : MonoBehaviour {
+public class ReCenter : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    // Use this for initialization
+    void Start()
+    {
+
+        UnityEngine.VR.InputTracking.Recenter();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.F12))
+        {
+            UnityEngine.VR.InputTracking.Recenter();
+        }
+    }
 }

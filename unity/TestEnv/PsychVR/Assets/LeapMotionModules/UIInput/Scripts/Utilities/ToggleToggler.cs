e60c2cdfa4154be893dfde3ada0c867b45d2c1bc
Zenairo <peterlomason@gmail.com>
2016-09-25 16:26:31 -0400

    elevator and misc fixes

--- unity/TestEnv/PsychVR/Assets/LeapMotionModules/UIInput/Scripts/Utilities/ToggleToggler.cs
+++ unity/TestEnv/PsychVR/Assets/LeapMotionModules/UIInput/Scripts/Utilities/ToggleToggler.cs
@@ -11,12 +11,10 @@ namespace Leap.Unity.InputModule {
 
     public void SetToggle(Toggle toggle) {
       if (toggle.isOn) {
-                GameObject.FindGameObjectsWithTag("toggle_obj")[0].GetComponent<Animator>().SetBool(text.text, true);
         text.color = Color.white;
         image.color = OnColor;
       } else {
-                GameObject.FindGameObjectsWithTag("toggle_obj")[0].GetComponent<Animator>().SetBool(text.text, false);
-                text.color = new Color(0.3f, 0.3f, 0.3f);
+        text.color = new Color(0.3f, 0.3f, 0.3f);
         image.color = OffColor;
       }
     }

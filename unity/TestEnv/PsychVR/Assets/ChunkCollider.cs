b703931c9609d2f80f385e6decf694966d029d77
Eric Momper <emomper23@knights.ucf.edu>
2016-10-21 12:22:19 -0400

    repeating terrain

--- unity/TestEnv/PsychVR/Assets/ChunkCollider.cs
+++ unity/TestEnv/PsychVR/Assets/ChunkCollider.cs
@@ -3,13 +3,15 @@ using System.Collections;
 
 public class ChunkCollider : MonoBehaviour {
 
+    public GameObject terrain_manager;
 	// Use this for initialization
 	void Start () {
 	
 	}
     void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.transform.name);
+        Debug.Log(other.transform.name + " from"+ this.transform.position);
+       terrain_manager.GetComponent<PlaneRepeat>().setPlane(this.gameObject);
     }
 
     // Update is called once per frame
--- unity/TestEnv/PsychVR/Assets/PlaneRepeat.cs
+++ unity/TestEnv/PsychVR/Assets/PlaneRepeat.cs
@@ -6,9 +6,9 @@ public class PlaneRepeat : MonoBehaviour {
     // Use this for initialization
     public Transform char_position;
     public GameObject plane;
-    private GameObject[] plane_list;
+    private GameObject[] plane_list = new GameObject[9];
     public GameObject player;
-    public float scale = 10;
+    public float scale = 100;
     public float GRID_X = 3;
     public float GRID_Z = 3;
 
@@ -21,20 +21,30 @@ public class PlaneRepeat : MonoBehaviour {
 
         float gx = -1;
         float gz = -1;
+        foreach(GameObject p in plane_list)
+        {
+            GameObject.Destroy(p);
+        }
         Quaternion rot = plane.transform.rotation;
         for(int i = 0; i < GRID_X; i++)
         {
             gz = -1;
             for (int j = 0; j < GRID_Z; j++)
             {
-                GameObject.Instantiate(plane, new Vector3(pos_x + size_x * gx, 0, pos_z + size_z * gz), rot);
+                plane.transform.name = "plane";
+                plane_list[i * 3 + j] = (GameObject)GameObject.Instantiate(plane, new Vector3(pos_x + size_x * gx, 0, pos_z + size_z * gz), rot);
+                
                 gz++;
             }
             gx++;
         }
         
 	}
-	
+    public void setPlane(GameObject obj)
+    {
+        plane = obj;
+        Start();
+    }
 	// Update is called once per frame
 	void Update ()
     {

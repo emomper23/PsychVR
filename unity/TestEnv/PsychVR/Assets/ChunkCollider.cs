65065ca0d2d67a7b3d838cb529de149c2f4f9012
Eric Momper <emomper23@knights.ucf.edu>
2016-10-20 22:57:36 -0400

    added 2 files

---
+++ unity/TestEnv/PsychVR/Assets/ChunkCollider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChunkCollider : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.transform.name);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	}
+}

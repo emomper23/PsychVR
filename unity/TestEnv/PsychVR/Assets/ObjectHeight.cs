9ac4b6f6101394a0587289a69ae0e3c8cf63fc32
Eric Momper <emomper23@knights.ucf.edu>
2016-11-02 00:02:03 -0400

    trees add.

---
+++ unity/TestEnv/PsychVR/Assets/ObjectHeight.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectHeight : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake ()
+    {
+        Vector3 tree_pos = this.transform.position;
+        Ray check = new Ray(tree_pos, Vector3.down);
+        RaycastHit test;
+        Physics.Raycast(check, out test,100.0f);
+        Debug.Log(test.collider.gameObject.transform.name);
+
+        this.transform.position = tree_pos - new Vector3(0, test.distance, 0);
+
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+	
+	}
+}
